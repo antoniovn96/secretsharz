0a63e8f744915df512847f73b733f4427218cca0
antoniovn96 <88204286+antoniovn96@users.noreply.github.com>
2026-08-10 13:33:08 +0530

    design-system: wire shared header migration styles globally

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import '../src/design-system/tokens.css';
 import '../src/design-system/DesignSystem.css';
+import '../src/design-system/header-migration.css';
 import '../src/styles/StudentDashboard.css';
 
 export default function App({ Component, pageProps }) {
