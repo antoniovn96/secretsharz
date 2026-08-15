2ec556cff465df316156376eccd4e555e1422f0e
antoniovn96 <88204286+antoniovn96@users.noreply.github.com>
2026-08-15 18:06:04 +0530

    Add career profile route

---
+++ pages/dashboard/career/profile.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import CareerProfilePortfolio from '../../../src/dashboards/student/CareerProfilePortfolio';
+
+export default function CareerProfilePage() {
+  return <CareerProfilePortfolio />;
+}
