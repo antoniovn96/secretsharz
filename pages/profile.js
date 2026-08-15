f516966ec21d31a775efda5f8fa6db9e09117878
antoniovn96 <88204286+antoniovn96@users.noreply.github.com>
2026-08-15 18:05:47 +0530

    Add student profile page

---
+++ pages/profile.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import CareerProfilePortfolio from '../src/dashboards/student/CareerProfilePortfolio';
+
+export default function ProfilePage() {
+  return <CareerProfilePortfolio />;
+}
