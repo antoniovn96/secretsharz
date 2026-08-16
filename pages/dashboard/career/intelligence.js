fac320a6b551e7fb3e3b176382946365d6ac4f76
antoniovn96 <88204286+antoniovn96@users.noreply.github.com>
2026-08-16 17:05:54 +0530

    feat(career): expose Career Intelligence dashboard

---
+++ pages/dashboard/career/intelligence.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import CareerIntelligenceDashboard from '../../../src/dashboards/student/CareerIntelligenceDashboard';
+
+export default function CareerIntelligencePage() {
+  return <CareerIntelligenceDashboard />;
+}
