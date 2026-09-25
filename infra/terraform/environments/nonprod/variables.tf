caa3cfbe473f928490ddf52e038ebcb5408b4adf
antoniovn96 <88204286+antoniovn96@users.noreply.github.com>
2026-09-25 14:19:00 +0530

    infra: configure nonprod network CIDR

--- infra/terraform/environments/nonprod/variables.tf
+++ infra/terraform/environments/nonprod/variables.tf
@@ -8,3 +8,9 @@ variable "aws_region" {
     error_message = "The current SecretSharz non-production project is intentionally pinned to Sydney (ap-southeast-2)."
   }
 }
+
+variable "vpc_cidr" {
+  description = "Private application database VPC CIDR."
+  type        = string
+  default     = "10.40.0.0/16"
+}
