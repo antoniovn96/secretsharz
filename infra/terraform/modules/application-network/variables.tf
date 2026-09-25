e3b1af4db66f5b5ec7941f005f8b559c0d8bded5
antoniovn96 <88204286+antoniovn96@users.noreply.github.com>
2026-09-25 14:21:33 +0530

    infra: add public private and NAT network controls

--- infra/terraform/modules/application-network/variables.tf
+++ infra/terraform/modules/application-network/variables.tf
@@ -15,3 +15,18 @@ variable "az_count" {
     error_message = "az_count must be between 2 and 6."
   }
 }
+
+variable "nat_gateway_count" {
+  type    = number
+  default = 1
+
+  validation {
+    condition     = var.nat_gateway_count >= 1 && var.nat_gateway_count <= 6
+    error_message = "nat_gateway_count must be between 1 and 6."
+  }
+}
+
+variable "enable_nat" {
+  type    = bool
+  default = true
+}
