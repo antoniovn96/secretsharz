011c3e1699514e11fb3054f01022b195c125753b
antoniovn96 <88204286+antoniovn96@users.noreply.github.com>
2026-09-25 20:09:53 +0530

    infra: expose migration execution role output

--- infra/terraform/environments/nonprod/main.tf
+++ infra/terraform/environments/nonprod/main.tf
@@ -210,3 +210,7 @@ output "assessment_migration_runner_task_definition_arn" {
 output "assessment_migration_runner_container_name" {
   value = "secretsharz-nonprod-assessment-migrations"
 }
+
+output "assessment_migration_runner_task_execution_role_arn" {
+  value = module.assessment_migration_runner.task_execution_role_arn
+}
