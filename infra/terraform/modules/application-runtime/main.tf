dee891bc0a4a0e8e6d3736d18a7c2f45ceae249f
antoniovn96 <88204286+antoniovn96@users.noreply.github.com>
2026-09-25 19:26:21 +0530

    infra: require TLS for ECS PostgreSQL connections

--- infra/terraform/modules/application-runtime/main.tf
+++ infra/terraform/modules/application-runtime/main.tf
@@ -203,7 +203,16 @@ resource "aws_ecs_task_definition" "this" {
       protocol      = "tcp"
     }]
 
-    environment = var.environment_variables
+    environment = concat([
+      {
+        name  = "DATABASE_SSL"
+        value = "true"
+      },
+      {
+        name  = "DATABASE_SSL_REJECT_UNAUTHORIZED"
+        value = "true"
+      }
+    ], var.environment_variables)
 
     secrets = [
       for item in var.secret_environment_variables : {
