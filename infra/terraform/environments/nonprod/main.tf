4076e6ea2da580157b36885c27de417a2e92dba8
antoniovn96 <88204286+antoniovn96@users.noreply.github.com>
2026-09-25 14:22:06 +0530

    infra: add nonprod ECR repository

--- infra/terraform/environments/nonprod/main.tf
+++ infra/terraform/environments/nonprod/main.tf
@@ -66,3 +66,14 @@ output "postgres_master_user_secret_arn" {
 output "application_security_group_id" {
   value = module.network.application_security_group_id
 }
+
+
+module "container_registry" {
+  source = "../../modules/container-registry"
+
+  name = "secretsharz-nonprod"
+}
+
+output "container_registry_url" {
+  value = module.container_registry.repository_url
+}
