99dd90a41286f0531d8cb47a9f8cbaad2d6c09a6
antoniovn96 <88204286+antoniovn96@users.noreply.github.com>
2026-09-25 19:28:31 +0530

    infra: enable verified PostgreSQL TLS in nonprod

--- infra/terraform/environments/nonprod/main.tf
+++ infra/terraform/environments/nonprod/main.tf
@@ -99,6 +99,14 @@ module "application_runtime" {
     {
       name  = "PORT"
       value = "3000"
+    },
+    {
+      name  = "DATABASE_SSL"
+      value = "true"
+    },
+    {
+      name  = "DATABASE_SSL_REJECT_UNAUTHORIZED"
+      value = "true"
     }
   ]
 }
